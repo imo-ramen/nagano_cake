3938bb0dcecaa221c933c031b0be8c87a8413192
gontamooooon <97084855+gontamooooon@users.noreply.github.com>
2022-02-17 07:15:15 +0000

    [ADD]model,item_image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destory
+  has_many :orders_details, dependent: :destroy
+  belongs_to :genre
+
+  has_one_attached :item_image
+  def get_item_imege
+    item_image.attached? ? item_image : 'no_image.jpg'
+  end
 end
