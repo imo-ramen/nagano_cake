837bc79b33ce29cb754924ba9070f2ba301b5494
gontamooooon <97084855+gontamooooon@users.noreply.github.com>
2022-02-18 15:08:53 +0000

    [ADD]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,34 @@
 class Admin::ItemsController < ApplicationController
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
+  end
+  
+  def index
+    @items = Item.all
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  private
+  # ストロングパラメータ
+  def item_params
+    params.require(:item).permit(:item_image, :name, :introduction, :price)
+  end
 end
