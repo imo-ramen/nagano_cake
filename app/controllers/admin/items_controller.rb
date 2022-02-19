96448fae5d27ddfb79c2f5be1a7ec0aeb880a5b4
gontamooooon <97084855+gontamooooon@users.noreply.github.com>
2022-02-19 14:32:43 +0000

    [ADD]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,9 +18,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
