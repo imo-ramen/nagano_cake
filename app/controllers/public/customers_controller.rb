af3f65984ecad5746708f71e7cfdb46d4b4fcd60
okumura-k <kh.0262.ok@gmail.com>
2022-02-19 11:22:37 +0000

    [add]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,35 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
   #ログインしている場合のみ表示。以外ならログイン画面遷移
-  
+
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
-  end 
-  
+  end
+
   def update
     @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path,notice: "You have updated user successfully."
+    else
+      render request.referer
+    end
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
