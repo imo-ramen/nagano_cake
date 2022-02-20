7b55a8a669043073af8e78c1ab2e916bfef96bfa
Koizumi1989 <08033431635a@gmail.com>
2022-02-20 13:47:23 +0000

    cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,48 +2,40 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @cart_items = current_customer.cart_items.all
+    @cart_items = current_customer.cart_items
   end
 
   def create
-    # formでのパラメーターの値がカラム名と違う場合 ストロングパラメーターにはカラムしかいれられないので、使えない。
-    # cart_item = CartItem.new
-    # cart_item.amount = params[:cart_item][:data1]
-    # cart_item.item_id = params[:cart_item][:data2]
-    # cart_item.customer_id = current_customer.id
-    # cart_item.save
-    # redirect_to ~~
-
-    # formでのパラメーターの値がカラム名と同じ場合
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    cart_item.save
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    cart_items = current_customer.cart_items
+    cart_items.each do |cart_item|
+      if cart_item.item_id == @cart_item.item_id
+        new_amount = cart_item.amount + @cart_item.amount
+        cart_item.update(amount:new_amount)
+        @cart_item.destroy
+      end
+    end
+    @cart_item.save
     redirect_to cart_items_path
-    
-    # @cart_item = current_.cart_customer.items.new(cart_item_params)
-    # if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-    #   @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-    #   cart_item.quantity += params[:cart_item][:quantity].to_i
-    #   cart_item.save
-    #   redirect_to cart_items_path
-    # elsif
-    #   @cart_item.save
-    #   @cart_items = current_member.cart_items.all
-    #   render 'index'
-    # else
-    #   render 'index'
-    # end
+  end
+
+  def update
+    @cart_items = CartItem.find(params[:id])
+    if @cart_items.update(cart_item_params)
+      redirect_to request.referer,notice: "You have updated user successfully."
+    end
   end
 
   def destroy
-    @cart_items = current_customer.cart_item.find(params[:id])
-    @cart_items.destroy
+    cart_items = CartItem.find(params[:id])
+    cart_items.destroy
     redirect_to request.referer
   end
 
   def destroy_all
-    @cart_items = current_customer.cart_items
-    @cart_items.destroy_all
+    cart_items = CartItem.all
+    cart_items.destroy_all
     redirect_to request.referer
   end
 
