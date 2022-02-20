e366747b0252a5e52ebf23601eb114e5cf38296c
gontamooooon <97084855+gontamooooon@users.noreply.github.com>
2022-02-20 07:14:52 +0000

    [ADD]customer/index,show,edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,22 +1,30 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def edit
+    @customer = Customer.find(params[:id])
   end
-  
+
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = 'You have updated customer successfully.'
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
-  
+
   private
-  
+
   def customer_params
-     params.require(:customer).permit(:item_image, :name, :introduction, :price, :genre_id, :is_active)
+     params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
