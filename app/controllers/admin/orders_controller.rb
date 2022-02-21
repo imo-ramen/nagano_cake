a6e90b78a1de60f71fe91f3a7d3314a7d82f979d
gontamooooon <97084855+gontamooooon@users.noreply.github.com>
2022-02-21 14:28:52 +0000

    [ADD]order/show,order_details

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,22 @@
 class Admin::OrdersController < ApplicationController
+  
+ def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+ end
+ 
+ def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to request.referer
+    else
+      render :show
+    end
+ end
+ 
+ private
+  # ストロングパラメータ
+  def order_params
+    params.require(:order).permit(:name, :postal_code, :address, :payment_method, :status)
+  end
 end
