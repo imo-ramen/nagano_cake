32fa7bf24015316e6d285be2ef79099f6cabe14a
Koizumi1989 <08033431635a@gmail.com>
2022-02-21 14:28:37 +0000

    etc2

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,8 @@ class Order < ApplicationRecord
   has_many :orders_details, dependent: :destroy
   belongs_to :customer
 
+  # belongs_to :follower, class_name: "User"
+  # belongs_to :followed, class_name: "User"
+
   enum payment_method: { credit_card: 0, transfer: 1 }
 end
