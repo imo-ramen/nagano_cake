6f39dfd759332e190cf0b015f22dc903e5a37092
okumura-k <kh.0262.ok@gmail.com>
2022-02-23 05:37:41 +0000

    fin

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -17,6 +17,6 @@ class Admin::OrderDetailsController < ApplicationController
  private
 
  def order_detail_params
-    params.require(:order_detail).permit(:making_status)
-  end
+    params.require(:order_detail).permit(:making_status) 
+ end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,19 +2,11 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum making_status: {
+  enum status: {
       製作不可: 0,
       製作待ち: 1,
       製作中: 2,
       製作完了: 3,
    }
-
-  def  add_tax_price
-    (self.price * 1.10).round
-  end
-
-  def subtotal
-    self.item.with_tax_price * amount
-  end
 
 end
