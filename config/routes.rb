db03d0c8d1c7e855e5b7ef64b5f62dab02936eed
Koizumi1989 <08033431635a@gmail.com>
2022-02-23 03:03:11 +0000

    残り2ばつ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+
+
+
   root to: "public/homes#top"
   get "about" =>"public/homes#about"
 
@@ -43,13 +46,14 @@ Rails.application.routes.draw do
   end
   end
 
-  devise_for :admins,skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
-
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
   }
+
+  devise_for :admins,skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
