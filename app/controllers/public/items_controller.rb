090598953a4db550df442cee2feda8a8cf92193c
Koizumi1989 <08033431635a@gmail.com>
2022-02-23 06:24:12 +0000

    Finish2

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,13 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @items = Item.all
+    @items = Item.where(is_active: true)
   end
 
   def show
     @item = Item.find(params[:id])
+    redirect_to items_path and return unless @item.is_active
     @cart_item = CartItem.new
   end
-
 
 end
