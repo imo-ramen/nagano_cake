0a99fe216c2c9c29ebcd22379a58df017f3b8bf1
Koizumi1989 <08033431635a@gmail.com>
2022-02-24 07:41:23 +0000

    退会

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,5 +7,9 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
 
+  def active_for_authentication?
+    super && (self.is_deleted == false)
+  end
+
   validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, presence: :true
 end
